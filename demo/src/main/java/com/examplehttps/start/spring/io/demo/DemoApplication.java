8cc2b8c840f947e2245430f763f4993bcb631c47
unknown <aleron@kaist.ac.kr>
2021-07-23 22:26:32 +0900

    cannot find symbol class Post

--- demo/src/main/java/com/examplehttps/start/spring/io/demo/DemoApplication.java
+++ demo/src/main/java/com/examplehttps/start/spring/io/demo/DemoApplication.java
@@ -3,11 +3,20 @@ package com.examplehttps.start.spring.io.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoApplication {
 
+	private int i = 1;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		//SpringApplication application = new SpringApplication(DemoApplication.class);
+		//application.run(args);
+		System.out.println("hello world");
+		Post post = new Post();
+		//System.out.println(postsController.readPosts());
+		System.out.println("fffffff");
 	}
 
 }
